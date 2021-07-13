0b772b78daea83f630e21f80b4886438dce2d959
Nguyen Ngoc Tuan <tuandev1996@gmail.com>
2021-07-14 00:02:47 +0700

    add project

---
+++ component/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+    Image,
+    Text,
+    TextInput,
+    Button,
+    View
+} from 'react-native'
+
+export default function Login (props) {
+    return <View>
+        <Text>Đăng nhập</Text>
+    </View>
+}
